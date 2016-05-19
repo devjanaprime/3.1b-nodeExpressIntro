1279f697ea7bf8c8694f7ea1465c1e2461bb3f2b
devjanaprime <dev@primeacademy.io>
2016-05-19 12:28:18 -0500

    added function response text

--- server/app.js
+++ server/app.js
@@ -3,7 +3,12 @@ var http = require('http');
 http.createServer( function( req, res ){
   res.writeHead( 200 );
   res.write( "hello world from create server response" );
+  res.write( functionResponse() );
   res.end();
 }).listen(3000);
+
+var functionResponse = function(){
+  return( " ...and this is some text from a function!");
+}
 
 console.log( 'server listening on port 3000' );
