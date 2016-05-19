bceeaa4913f0d04c80ee05825e251e42c04f8107
Dev Jana <devjana@Devs-MacBook-Pro.local>
2016-05-19 12:15:10 -0500

    app,js changed...

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
-http.createServer( function( req, res ) {
-  console.log( "hello world from server on start" );
+http.createServer( function( req, res )
+  res.writeHead( 200 );
+  res.write( "hello world from response" );
+  res.end();
 }).listen(3000);
 
 console.log( 'server listening on port 3000' );
